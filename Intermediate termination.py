4e844e0e05da05acadef241b65df2f91d2775689
avinash4tripathi <avinash.t@diggibyte.com>
2026-08-31 18:37:38 +0530

    commit continue,break and pass statement

---
+++ Intermediate termination.py
@@ -0,0 +1,15 @@
+# Break
+'''for i in  range(1,6):
+    if i == 2:
+        break
+    print(i)'''
+
+# Continue
+i = 1
+end = 5
+while i<= end:
+  if i == 3:
+        continue
+  print(i)
+  i += 1
+
